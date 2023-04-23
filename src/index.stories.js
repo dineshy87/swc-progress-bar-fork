bf3d5e9d7ba78be586a56f8dbeba7ff045e5364a
Dinesh Yalagandula <16927765+undefined@users.noreply.github.com>
2023-04-23 07:56:34 +0000

    Simplified story

--- src/index.stories.js
+++ src/index.stories.js
@@ -1,9 +1,71 @@
-import { swcThemeDecorator } from "@spectrum-web-components/story-decorator/decorator.js";
 import { html } from "lit-html";
 import "./index.ts";
+import "@spectrum-web-components/theme/sp-theme.js";
+import "@spectrum-web-components/theme/src/themes.js";
+import "@spectrum-web-components/theme/src/express/themes.js";
 
-export default {
-  decorators: [swcThemeDecorator],
-};
+const customStyles = html`
+  <style>
+  sp-theme {
+              overflow-x: hidden;
+              display: block;
+              box-sizing: border-box;
+              width: 100%;
+              min-height: 40vh;
+              padding: var(--spectrum-global-dimension-size-100)
+                  var(--spectrum-global-dimension-size-100)
+                  calc(
+                      2 * var(--spectrum-alias-focus-ring-size) +
+                          var(--spectrum-alias-item-height-m)
+                  );
+              box-sizing: border-box;
+              background-color: var(--spectrum-global-color-gray-100);
+              color: var(
+                  --spectrum-body-text-color,
+                  var(--spectrum-alias-text-color)
+              );
+            }
+  </style>
+`;
 
-export const story1 = () => html`<my-progress-bar></my-progress-bar> `;
+export const story1 = () => html`
+${customStyles}
+  <sp-theme
+    theme="spectrum"
+    color="lightest"
+    scale="medium"
+    dir="ltr"
+  >
+    <my-progress-bar></my-progress-bar>
+  </sp-theme>
+
+  <sp-theme
+    theme="spectrum"
+    color="light"
+    scale="medium"
+    dir="ltr"
+    part="container"
+  >
+    <my-progress-bar></my-progress-bar>
+  </sp-theme>
+
+  <sp-theme
+    theme="spectrum"
+    color="dark"
+    scale="medium"
+    dir="ltr"
+    part="container"
+  >
+    <my-progress-bar></my-progress-bar>
+  </sp-theme>
+
+  <sp-theme
+    theme="spectrum"
+    color="darkest"
+    scale="medium"
+    dir="ltr"
+    part="container"
+  >
+    <my-progress-bar></my-progress-bar>
+  </sp-theme>
+`;

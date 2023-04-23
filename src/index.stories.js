4958ad5bd8d49618f7e9a981bfa7b289f3fb383d
Dinesh Yalagandula <dinesh.yalagandula87@gmail.com>
2023-04-22 23:43:08 -0700

    Delete index.stories.js

--- src/index.stories.js
+++
@@ -1,9 +0,0 @@
-import { swcThemeDecorator } from "@spectrum-web-components/story-decorator/decorator.js";
-import { html } from "lit-html";
-import "./index.ts";
-
-export default {
-  decorators: [swcThemeDecorator],
-};
-
-export const story1 = () => html`<my-progress-bar></my-progress-bar> `;

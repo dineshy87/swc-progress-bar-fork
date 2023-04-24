bf8c780f50c1e9b02a8c301310f63b5b0d96b2d5
Dinesh Yalagandula <16927765+undefined@users.noreply.github.com>
2023-04-24 23:44:41 +0000

    Formatting and adding different combinations for labelling

--- src/index.ts
+++ src/index.ts
@@ -26,8 +26,8 @@ export default class ProgressAndMeter extends LitElement {
         justify-content: space-around;
       }
 
-      sp-progress-bar { 
-        padding: 22px;
+      sp-progress-bar, sp-meter { 
+        padding: 18px;
       }
 
       
@@ -38,15 +38,12 @@ export default class ProgressAndMeter extends LitElement {
   protected render(): TemplateResult {
     return html`
     <h2>Progress Bar</h2>
-      <sp-progress-bar label="Loaded a lot" progress="71" small></sp-progress-bar>
-      <sp-progress-bar
-        label="Loaded indeterminately"
-        indeterminate
-      ></sp-progress-bar>
-      <sp-progress-bar
-        aria-label="Using aria-label"
-        indeterminate
-      ></sp-progress-bar>
+      <sp-progress-bar label="Using label" progress="71" small></sp-progress-bar>
+      <sp-progress-bar label="Using label" indeterminate></sp-progress-bar>
+      <sp-progress-bar aria-label="Using aria-label" indeterminate></sp-progress-bar>
+      <sp-progress-bar progress="71" small>Using text within</sp-progress-bar>
+      <sp-progress-bar label="Am from label" progress="79" small>Am text within</sp-progress-bar>
+      
       <div
           style="width: var(--spectrum-global-dimension-size-3000); height: var(--spectrum-global-dimension-size-2000); display: flex; flex-direction: column; align-items: center; justify-content: space-around; background-color: var(--spectrum-alias-background-color-modal-overlay);"
       >
@@ -59,19 +56,28 @@ export default class ProgressAndMeter extends LitElement {
 
       <h2>Progress Circle</h2>
 
-      <div style="width: 250px; height: 150px; display: flex; align-items: center; justify-content: space-around;"><sp-progress-circle label="A small representation of a somewhat completed action" progress="71" size="s"></sp-progress-circle><sp-progress-circle label="A medium representation of a recently started action" progress="22"></sp-progress-circle><sp-progress-circle label="A large representation of an almost completed action" progress="86" size="l"></sp-progress-circle></div>
-      <div style="width: 250px; height: 150px; display: flex; align-items: center; justify-content: space-around;  background-color: rgba(0,0,0,0.4);"><sp-progress-circle label="A small representation of a partially completed action" progress="42" static="white" size="s"></sp-progress-circle> <sp-progress-circle label="A medium representation of a barely started action" progress="7" static="white"></sp-progress-circle><sp-progress-circle label="A large representation of a somewhat completed action" progress="68" static="white" size="l"></sp-progress-circle></div>
-      <div style="width: 250px; height: 150px; display: flex; align-items: center; justify-content: space-around;"><sp-progress-circle label="A small representation of an unclear amount of work" indeterminate size="s"></sp-progress-circle><sp-progress-circle label="A medium representation of an unclear amount of work" indeterminate></sp-progress-circle><sp-progress-circle label="A large representation of an unclear amount of work" indeterminate size="l"></sp-progress-circle></div>
-      
+      <div style="width: 250px; height: 150px; display: flex; align-items: center; justify-content: space-around;">
+        <sp-progress-circle label="Small progress circle" progress="71" size="s"></sp-progress-circle>
+        <sp-progress-circle progress="22">Medium progress circle</sp-progress-circle>
+        <sp-progress-circle label="Large progress circle" progress="86" size="l"></sp-progress-circle>
+      </div>
+      <div style="width: 250px; height: 150px; display: flex; align-items: center; justify-content: space-around;  background-color: rgba(0,0,0,0.4);">
+        <sp-progress-circle label="Small progress circle" progress="42" static="white" size="s"></sp-progress-circle>
+        <sp-progress-circle label="Medium progress circle" progress="7" static="white"></sp-progress-circle>
+        <sp-progress-circle label="Large progress circle" progress="68" static="white" size="l"></sp-progress-circle>
+      </div>
+      <div style="width: 250px; height: 150px; display: flex; align-items: center; justify-content: space-around;">
+        <sp-progress-circle label="Small progress circle" indeterminate size="s"></sp-progress-circle>
+        <sp-progress-circle label="Medium progress circle" indeterminate></sp-progress-circle>
+        <sp-progress-circle label="Large progress circle" indeterminate size="l"></sp-progress-circle>
+      </div>
       <h2>Meter</h2>
 
-      <sp-meter size="m" progress="71">Tasks Completed</sp-meter>
+      <sp-meter size="m" progress="71" label="Using label"></sp-meter>
+      <sp-meter size="m" progress="71">Using text within</sp-meter>
       <div style="width: var(--spectrum-global-dimension-size-3000); height: var(--spectrum-global-dimension-size-2000); display: flex; flex-direction: column; align-items: center; justify-content: space-around; background-color: var(--spectrum-alias-background-color-modal-overlay); margin-top: 12px;">
-        <sp-meter progress="42" over-background>Tasks Completed</sp-meter>
+        <sp-meter progress="42" over-background label="Tasks Completed">Using both</sp-meter>
       </div>
-
-
-
     `;
   }
 }
